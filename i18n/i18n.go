cd76cb2075fc22f440f65c008338db1c736cb691
f6o <25875630+f6o@users.noreply.github.com>
2026-03-04 09:10:40 +0900

    Refactor i18n: embed locale data and simplify locale loading by removing filesystem-based detection; default to en-US using embedded locale data.

--- i18n/i18n.go
+++ i18n/i18n.go
@@ -1,28 +1,27 @@
 package i18n
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
 	"sync"
+
+	_ "embed"
 
 	"unknwon.dev/i18n"
 )
 
+//go:embed locales/locale_en-US.ini
+var localeData []byte
+
 var (
-	store  *i18n.Store
-	locale string
-	once   sync.Once
+	store *i18n.Store
+	once  sync.Once
 )
 
 func Init() *i18n.Store {
 	once.Do(func() {
 		s := i18n.NewStore()
-		locale = DetectLocale()
 
-		dir := findLocaleFile()
-
-		_, err := s.AddLocale("en-US", "English", dir)
+		// TODO: fall back to `en-US` for now
+		_, err := s.AddLocale("en-US", "English", localeData)
 		if err != nil {
 			panic("failed to load locale: " + err.Error())
 		}
@@ -30,61 +29,6 @@ func Init() *i18n.Store {
 		store = s
 	})
 	return store
-}
-
-func findLocaleFile() string {
-	paths := []string{
-		"i18n/locales/locale_en-US.ini",
-		"../i18n/locales/locale_en-US.ini",
-		"../../i18n/locales/locale_en-US.ini",
-	}
-
-	exePath, err := os.Executable()
-	if err == nil {
-		exeDir := filepath.Dir(exePath)
-		paths = append(paths,
-			filepath.Join(exeDir, "i18n/locales/locale_en-US.ini"),
-			filepath.Join(exeDir, "../i18n/locales/locale_en-US.ini"),
-		)
-	}
-
-	for _, p := range paths {
-		if abs, err := filepath.Abs(p); err == nil {
-			if _, err := os.Stat(abs); err == nil {
-				return abs
-			}
-		}
-	}
-
-	return paths[0]
-}
-
-func DetectLocale() string {
-	lang := os.Getenv("LANG")
-	if lang == "" {
-		lang = os.Getenv("LC_ALL")
-	}
-	if lang == "" {
-		return "en-US"
-	}
-	return normalizeLocale(lang)
-}
-
-func normalizeLocale(l string) string {
-	l = strings.TrimSpace(l)
-	l = strings.ReplaceAll(l, "_", "-")
-	l = strings.Split(l, ".")[0]
-	if l == "" {
-		return "en-US"
-	}
-	return l
-}
-
-func CurrentLocale() string {
-	if locale == "" {
-		Init()
-	}
-	return locale
 }
 
 func T(key string, args ...interface{}) string {
