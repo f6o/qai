d2d8fdbbc15a38e60b6298be2705d2aca854b814
f6o <25875630+f6o@users.noreply.github.com>
2026-09-09 19:56:22 +0900

    feat: add --json output to idea list and todo list commands

--- cmd/idea_list.go
+++ cmd/idea_list.go
@@ -1,15 +1,24 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/f6o/qai/i18n"
+	"github.com/f6o/qai/internal/model"
 	"github.com/spf13/cobra"
 )
 
+var ideaListJSON bool
+
 var ideaListCmd = &cobra.Command{
-	Use:   "list",
-	Short: i18n.T("cmd.idea_list.short"),
+	Use:          "list",
+	Short:        i18n.T("cmd.idea_list.short"),
+	SilenceUsage: true,
+	Long: `List all ideas.
+Use --json to get exactly one machine-readable line: a JSON array of idea records.`,
+	Example: `  qai idea list
+  qai idea list --json | jq -r '.[].id'`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx, err := NewAppContext()
 		if err != nil {
@@ -22,6 +31,14 @@ var ideaListCmd = &cobra.Command{
 		}
 
 		ideas := ctx.TaskStore.FilterIdeas(tasks)
+
+		if ideaListJSON {
+			if ideas == nil {
+				ideas = []model.Task{}
+			}
+			return json.NewEncoder(cmd.OutOrStdout()).Encode(ideas)
+		}
+
 		if len(ideas) == 0 {
 			cmd.Println(i18n.T("cmd.idea_list.empty"))
 			return nil
@@ -37,4 +54,5 @@ var ideaListCmd = &cobra.Command{
 
 func init() {
 	ideaCmd.AddCommand(ideaListCmd)
+	ideaListCmd.Flags().BoolVar(&ideaListJSON, "json", false, i18n.T("cmd.idea_list.flag_json"))
 }
--- cmd/todo_list.go
+++ cmd/todo_list.go
@@ -1,16 +1,25 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"sort"
 
 	"github.com/f6o/qai/i18n"
+	"github.com/f6o/qai/internal/model"
 	"github.com/spf13/cobra"
 )
 
+var todoListJSON bool
+
 var todoListCmd = &cobra.Command{
-	Use:   "list",
-	Short: i18n.T("cmd.todo_list.short"),
+	Use:          "list",
+	Short:        i18n.T("cmd.todo_list.short"),
+	SilenceUsage: true,
+	Long: `List all todos.
+Use --json to get exactly one machine-readable line: a JSON array of todo records.`,
+	Example: `  qai todo list
+  qai todo list --json | jq -r '.[].id'`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx, err := NewAppContext()
 		if err != nil {
@@ -23,14 +32,22 @@ var todoListCmd = &cobra.Command{
 		}
 
 		todos := ctx.TaskStore.FilterTodos(tasks)
-		if len(todos) == 0 {
-			cmd.Println(i18n.T("cmd.todo_list.empty"))
-			return nil
-		}
 
 		sort.Slice(todos, func(i, j int) bool {
 			return todos[i].Priority > todos[j].Priority
 		})
+
+		if todoListJSON {
+			if todos == nil {
+				todos = []model.Task{}
+			}
+			return json.NewEncoder(cmd.OutOrStdout()).Encode(todos)
+		}
+
+		if len(todos) == 0 {
+			cmd.Println(i18n.T("cmd.todo_list.empty"))
+			return nil
+		}
 
 		cmd.Println(i18n.T("cmd.todo_list.header"))
 		for _, t := range todos {
@@ -42,4 +59,5 @@ var todoListCmd = &cobra.Command{
 
 func init() {
 	todoCmd.AddCommand(todoListCmd)
+	todoListCmd.Flags().BoolVar(&todoListJSON, "json", false, i18n.T("cmd.todo_list.flag_json"))
 }
